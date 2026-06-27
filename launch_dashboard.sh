33fa565c0d164df46728bdb13d15e73db8d64ac5
rpp5069-cpu <rpp5069@gmail.com>
2026-06-26 23:56:37 -0400

    Update launch_dashboard.sh

--- launch_dashboard.sh
+++ launch_dashboard.sh
@@ -1,13 +1,5 @@
-
 #!/usr/bin/env bash
-# Run this script from the repo root to serve the dashboard locally
-# Usage: bash launch_dashboard.sh
 set -e
-NOTEBOOK="CS_MBA_AI_USAJobs_Jooble_Master_Combiner_v3_(2).ipynb"
-echo "Starting Voilà dashboard on http://localhost:8866"
-voila "$NOTEBOOK" \
-  --no-browser=False \
-  --port=8866 \
-  --theme=light \
-  --strip_sources=True
-
+NOTEBOOK="CS_MBA_AI_USAJobs_Jooble_Master_Combiner_v3_Dashboard.ipynb"
+echo "Starting Voila dashboard on http://localhost:8866"
+voila "$NOTEBOOK" --no-browser=False --port=8866 --theme=light --strip_sources=True
